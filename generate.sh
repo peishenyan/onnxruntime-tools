51bcb346e4507e4af9b5760e3fa9d316333c6720
peishenyan <peishen.yan@intel.com>
2024-10-22 09:08:13 +0800

    Generation bash is verified.

--- generate.sh
+++ generate.sh
@@ -5,7 +5,7 @@ echo 'LOG: Export decode model finished!!!'
 model_path="logs/models/Phi_3_mini_4k_instruct/"
 model_list=("Phi_3_mini_4k_instruct_decoder_1_prefill" "Phi_3_mini_4k_instruct_decoder_2_decode")
 
-python convert.py -path ${model_list[0]}.onnx
+python convert.py -path ${model_path}${model_list[0]}.onnx
 echo 'LOG 1: Generating model with external data finished.'
 python opset_convert.py -path ${model_path}${model_list[0]}_ex.onnx -v 21
 echo 'LOG 1: Generating model with new opset finished.'
@@ -19,7 +19,7 @@ rm ${model_path}${model_list[0]}_ex_v21_INT4_QDQ.onnx*
 python rename.py -i ${model_path}${model_list[0]}_ex_v21_INT4_QDQ_padded.onnx -o ${model_path}1_prefill_INT4_padded.onnx -s 20000000
 echo 'LOG 1: Renaming model finished.'
 
-python convert.py -path ${model_list[1]}.onnx
+python convert.py -path ${model_path}${model_list[1]}.onnx
 echo 'LOG 2: Generating model with external data finished.'
 python opset_convert.py -path ${model_path}${model_list[1]}_ex.onnx -v 21
 echo 'LOG 2: Generating model with new opset finished.'
