906d2f94f34d188a7c1db7307b963a6ca2d5b598
peishenyan <peishen.yan@intel.com>
2024-10-31 11:13:44 +0800

    implement attention sink mechanism in pad

--- generate_dynamic.sh
+++ generate_dynamic.sh
@@ -1,18 +1,23 @@
 python decoder-only-dynamic.py -m microsoft/Phi-3-mini-4k-instruct
 echo 'LOG: Export dynamic model finished!!!'
-model_path="logs/models/dynamic/Phi_3_mini_4k_instruct/"
-temp_path="logs/models/dynamic/done/"
+model_path="logs/models/dynamic_1/Phi_3_mini_4k_instruct/"
+temp_path="logs/models/dynamic_1/done/"
 model_name="Phi_3_mini_4k_instruct_decoder_static_cache"
 
 python convert.py -path ${model_path}${model_name}.onnx
-echo 'LOG 1: Generating model with external data finished.'
+echo 'LOG: Generating model with external data finished.'
 python opset_convert.py -path ${model_path}${model_name}_ex.onnx -v 21
-echo 'LOG 1: Generating model with new opset finished.'
+echo 'LOG: Generating model with new opset finished.'
 rm ${model_path}${model_name}_ex.onnx*
 python quantize_int4.py -path ${model_path}${model_name}_ex_v21.onnx
-echo 'LOG 1: Quantizing model finished.'
+echo 'LOG: Quantizing model finished.'
 rm ${model_path}${model_name}_ex_v21.onnx*
+python pad_dynamic.py -path ${model_path}${model_name}_ex_v21_INT4_QDQ.onnx
+echo 'LOG: Padding model finished.'
+rm ${model_path}${model_name}_ex_v21_INT4_QDQ.onnx*
 
 mkdir ${temp_path}
-mv ${model_path}${model_name}_ex_v21_INT4_QDQ.onnx* ${temp_path}
+mv ${model_path}${model_name}_ex_v21_INT4_QDQ_final.onnx* ${temp_path}
 rm -r ${model_path}
+python rename.py -i ${temp_path}${model_name}_ex_v21_INT4_QDQ_final.onnx -o ${temp_path}Phi_3_mini_4k_instruct_decoder_static_cache_MatmulNbits.onnx
+rm ${temp_path}${model_name}_ex_v21_INT4_QDQ_final.onnx*
--- pad_dynamic.py
+++ pad_dynamic.py
@@ -7,6 +7,7 @@ import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-path', '--model', type=str, help="The Whisper model to convert", default='Qwen/Qwen2-0.5B-Instruct')
+parser.add_argument('-sink', '--attn_sink', type=int, help='The length of attention sink tokens', default=4)
 args = parser.parse_args()
 model_prefix = args.model.split('.onnx')[0]    
 model_name = model_prefix.split('/')[-1]
@@ -20,6 +21,7 @@ new_outputs = []
 
 indices = []
 
+attention_sink_len = args.attn_sink
 kv_shape = None
 kv_denote = None
 for tensor in graph.input:
@@ -28,7 +30,11 @@ for tensor in graph.input:
         kv_denote = [dim.dim_param for dim in tensor.type.tensor_type.shape.dim]
 
 zero_tensor = helper.make_tensor('value_zero', onnx.TensorProto.INT32, [], [0])
-graph.initializer.extend([zero_tensor])
+one_tensor = helper.make_tensor('value_one', onnx.TensorProto.INT64, [], [1])
+sink_len_tensor = helper.make_tensor('sink_len', onnx.TensorProto.INT32, [], [attention_sink_len])
+sink_len_int64_tensor = helper.make_tensor('sink_len_int64', onnx.TensorProto.INT64, [], [attention_sink_len])
+sink_range_tensor = helper.make_tensor('sink_range', onnx.TensorProto.INT32, [attention_sink_len], range(attention_sink_len))
+graph.initializer.extend([zero_tensor, one_tensor, sink_len_tensor, sink_len_int64_tensor, sink_range_tensor])
 
  # Get the value of max_cache_length and max_text_length
 shape_node_1 = helper.make_node(
@@ -37,7 +43,7 @@ shape_node_1 = helper.make_node(
     inputs=['attention_mask'],
     outputs=['attention_mask_shape'],
     start=-1
-)
+) # max_cache_length+max_text_length
 
 shape_node_2 = helper.make_node(
     'Shape',
@@ -45,14 +51,23 @@ shape_node_2 = helper.make_node(
     inputs=['input_ids'],
     outputs=['input_ids_shape'],
     start=-1
-)
+) # max_text_length
 
 max_cache_len_node = helper.make_node(
     'Sub',
     name='max_cache_len_sub',
     inputs=['attention_mask_shape', 'input_ids_shape'],
     outputs=['max_cache_len']
+) # max_cache_length
+
+# To implement attention sink cache, we should construct left gather indices for sink cache and right gather indices for the rest cache.
+right_cache_len_node = helper.make_node(
+    'Sub',
+    name='right_cache_len_sub',
+    inputs=['max_cache_len', 'sink_len_int64'],
+    outputs=['right_cache_len']
 )
+graph.node.extend([shape_node_1, shape_node_2, max_cache_len_node, right_cache_len_node])
 
 # Prepare range(max_cache_len), range(max_seq_len), and range(max_seq_len+max_cache_len)
 ones_node_1 = helper.make_node(
@@ -60,40 +75,35 @@ ones_node_1 = helper.make_node(
     name='one_of_attn_mask',
     inputs=['attention_mask_shape'],
     outputs=['attention_mask_ones'],
-    value=helper.make_tensor('value_one', onnx.TensorProto.INT32, [1], [1])
+    value=helper.make_tensor('value_1', onnx.TensorProto.INT32, [1], [1])
 )
-
 ones_node_2 = helper.make_node(
     'ConstantOfShape',
-    name='one_of_max_cache_len',
-    inputs=['max_cache_len'],
-    outputs=['max_cache_len_ones'],
-    value=helper.make_tensor('value_one', onnx.TensorProto.INT32, [1], [1])
+    name='one_of_right_cache_len',
+    inputs=['right_cache_len'],
+    outputs=['right_cache_len_ones'],
+    value=helper.make_tensor('value_1', onnx.TensorProto.INT32, [1], [1])
 )
-
 ones_node_3 = helper.make_node(
     'ConstantOfShape',
     name='one_of_input_ids_shape',
     inputs=['input_ids_shape'],
     outputs=['input_ids_shape_ones'],
-    value=helper.make_tensor('value_one', onnx.TensorProto.INT32, [1], [1])
+    value=helper.make_tensor('value_1', onnx.TensorProto.INT32, [1], [1])
 )
-
 range_node_1 = helper.make_node(
     'CumSum',
     name='range_of_attn_mask',
     inputs=['attention_mask_ones', 'value_zero'],
     outputs=['mask_range'],
+    reverse=1,
 )
-
-
 range_node_2 = helper.make_node(
     'CumSum',
-    name='range_of_max_cache_len',
-    inputs=['max_cache_len_ones', 'value_zero'],
-    outputs=['gather_range'],
+    name='range_of_right_cache_len',
+    inputs=['right_cache_len_ones', 'value_zero'],
+    outputs=['right_gather_range'],
 )
-
 range_node_3 = helper.make_node(
     'CumSum',
     name='range_of_input_ids_shape',
@@ -101,7 +111,7 @@ range_node_3 = helper.make_node(
     outputs=['mask_gather_range'],
     exclusive=1,
 ) # start from 0
-graph.node.extend([shape_node_1, shape_node_2, max_cache_len_node, ones_node_1, ones_node_2, ones_node_3, range_node_1, range_node_2, range_node_3])
+graph.node.extend([ ones_node_1, ones_node_2, ones_node_3, range_node_1, range_node_2, range_node_3])
 
 
 ### Slice attention mask
@@ -127,36 +137,95 @@ gather_node = helper.make_node(
 )
 graph.node.extend([cast_node, add_node, gather_node])
 
-# 
-mul_node = helper.make_node(
+# Calculate argmax for right attention mask and argmin for all attention mask 
+mul_node_1 = helper.make_node(
+    'Mul',
+    inputs=['attention_mask', 'mask_range'],
+    outputs=['attention_mask_pos']
+)
+
+mul_node_2 = helper.make_node(
     'Mul',
     inputs=['sliced_attention_mask', 'mask_gather_indices'],
-    outputs=['attention_mask_pos']
+    outputs=['right_attention_mask_pos']
 )
 
 argmax_node = helper.make_node(
     'ArgMax',
-    name='Gather_position',
-    inputs=['attention_mask_pos'],
+    name='Gather_end',
+    inputs=['right_attention_mask_pos'],
     outputs=['argmax_res'],
     axis=1,
     keepdims=0
 )
 
-cast_node = helper.make_node(
+argmin_node = helper.make_node(
+    'ArgMax',
+    name='Gather_start',
+    inputs=['attention_mask_pos'],
+    outputs=['min_one_pos'],
+    axis=1,
+    keepdims=0
+)
+graph.node.extend([mul_node_1, mul_node_2, argmax_node, argmin_node])
+
+cast_node_1 = helper.make_node(
     'Cast',
     name='gather_addition_cast',
     inputs=['argmax_res'],
-    outputs=['gather_addition'],
+    outputs=['right_gather_addition'],
     to=TensorProto.INT32
-)
+) # INT32 argmax_res
 
 add_node = helper.make_node(
     'Add',
-    inputs=['gather_range', 'gather_addition'],
-    outputs=['gather_indices']
+    name='add',
+    inputs=['argmax_res', 'value_one'],
+    outputs=['input_len']
+) # argmax_res + 1 = input_len
+
+min_node = helper.make_node(
+    'Min',
+    inputs=['input_len', 'min_one_pos'],
+    outputs=['left_gather_addition_0']
+) # left gather indices start at min(input_len, max(0,max_cache_len-start_len)) = min(input_len, min_one_pos)
+
+cast_node_2 = helper.make_node(
+    'Cast',
+    name='left_gather_addition_cast',
+    inputs=['left_gather_addition_0'],
+    outputs=['left_gather_addition'],
+    to=TensorProto.INT32
 )
-graph.node.extend([mul_node, argmax_node, cast_node, add_node])
+
+add_node_0 = helper.make_node(
+    'Add',
+    name='add_0',
+    inputs=['sink_range', 'left_gather_addition'],
+    outputs=['left_gather_indices']
+)
+
+add_node_1 = helper.make_node(
+    'Add',
+    name='add_1',
+    inputs=['right_gather_range', 'right_gather_addition'],
+    outputs=['right_gather_indices_0']
+)
+
+add_node_2 = helper.make_node(
+    'Add',
+    name='add_2',
+    inputs=['right_gather_indices_0', 'sink_len'],
+    outputs=['right_gather_indices']
+)
+
+concat_node = helper.make_node(
+    'Concat',
+    inputs=['left_gather_indices', 'right_gather_indices'],
+    outputs=['gather_indices'],
+    axis=0
+)
+graph.node.extend([cast_node_1, min_node, cast_node_2, add_node, add_node_0, add_node_1, add_node_2, concat_node])
 
 for i, output in enumerate(graph.output):
     if 'logits' in output.name:
@@ -165,7 +234,7 @@ for i, output in enumerate(graph.output):
         gather_node = helper.make_node(
             'Gather',
             name='logits_gather',
-            inputs=[output.name, 'gather_addition'],
+            inputs=[output.name, 'right_gather_addition'],
             outputs=['sliced_'+output.name],
             axis=1
         )
